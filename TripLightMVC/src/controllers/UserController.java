d49e9df3e4924b00a1338137baed10dda696af1e
Developer <stephen.e.chiang@gmail.com>
2017-04-03 17:32:32 -0600

    fixed merge conflicts

--- TripLightMVC/src/controllers/UserController.java
+++ TripLightMVC/src/controllers/UserController.java
@@ -19,16 +19,16 @@ import data.UserDAO;
 @Controller
 @SessionAttributes("sessionUser")
 public class UserController {
-	
+
 	@Autowired
 	private UserDAO userdao;
-	
+
 	@ModelAttribute("sessionUser")
 	public User user() {
 		return new User();
 	}
-	
-	@RequestMapping(value="updateUser.do", method=RequestMethod.POST)
+
+	@RequestMapping(value = "updateUser.do", method = RequestMethod.POST)
 	public ModelAndView updateUserWithInformationFromPage(@ModelAttribute("sessionUser") User user) {
 		ModelAndView mv = new ModelAndView();
 		userdao.updateUser(user.getId(), user);
@@ -36,41 +36,37 @@ public class UserController {
 		mv.addObject("sessionUser", user);
 		return mv;
 	}
-	@RequestMapping(value="updateUser.do", method=RequestMethod.GET)
+
+	@RequestMapping(value = "updateUser.do", method = RequestMethod.GET)
 	public ModelAndView updateUser(@ModelAttribute("sessionUser") User user) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("updateUser");
 		mv.addObject("sessionUser", user);
 		return mv;
 	}
-	@RequestMapping(value="createUser.do", method=RequestMethod.GET)
+
+	@RequestMapping(value = "createUser.do", method = RequestMethod.GET)
 	public ModelAndView createUser(@ModelAttribute("sessionUser") User user) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("createUser");
 		return mv;
 	}
-	
-	@RequestMapping(value="createUser.do", method=RequestMethod.POST)
-	public ModelAndView createUserWithInfoFromPage(@Valid User user, Errors errors, @RequestParam("countryCode") String countryCode) {
+
+	@RequestMapping(value = "createUser.do", method = RequestMethod.POST)
+	public ModelAndView createUserWithInfoFromPage(@Valid User user, Errors errors,
+			@RequestParam("countryCode") String countryCode) {
 		ModelAndView mv = new ModelAndView();
 		userdao.createUser(user);
-<<<<<<< HEAD
 
-		 if (errors.getErrorCount() != 0) {
-		      mv.setViewName("createUser");
-		      return mv;
-		    }
-			mv.setViewName("index");
-			mv.addObject("sessionUser", user);
+		if (errors.getErrorCount() != 0) {
+			mv.setViewName("createUser");
 			return mv;
-		  }
 		}
-	
-=======
-		mv.setViewName("userMain");
+		mv.setViewName("index");
 		mv.addObject("sessionUser", user);
 		return mv;
 	}
+
 	@RequestMapping(value="deleteUser.do", method=RequestMethod.POST)
 	public ModelAndView deleteUSer(@ModelAttribute("sessionUser") User user) {
 		ModelAndView mv = new ModelAndView();
@@ -81,4 +77,3 @@ public class UserController {
 		return mv;
 	}
 }
->>>>>>> 05652121b7b7064c7907637ae59271c9ea189b95
