c29615d1e7121438914a963959d17b9cc9ca44a1
Developer <stephen.e.chiang@gmail.com>
2017-04-03 10:15:52 -0600

    fixed test-servlet context for testing

--- TripLightMVC/test/ctrl/LoginControllerTest.java
+++ TripLightMVC/test/ctrl/LoginControllerTest.java
@@ -1,5 +1,68 @@
 package ctrl;
 
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import data.TripLightDAO;
+import data.UserDAO;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "../WEB-INF/Test-context.xml" })
+@WebAppConfiguration
+@Transactional
 public class LoginControllerTest {
 
+	@Autowired
+	private WebApplicationContext wac;
+
+	@Autowired
+	private UserDAO dao;
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Before
+	public void setUp() {
+		dao = (UserDAO) wac.getBean("UserDAO");	
+	}
+
+	@After
+	public void tearDown() {
+		dao = null;
+		em = null;
+		wac = null;
+	}
+	
+	@Test
+	public void test_isValidUser() {
+		try {
+			assertEquals("", dao.isValidUser("admin", "solarkisses"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	public void test_logoutClearsSession() {
+		
+	}
+	
 }
--- TripLightMVC/test/dao/TripLightDAOImpl.java
+++
@@ -1,5 +0,0 @@
-package dao;
-
-public class TripLightDAOImpl implements TripLightDAO {
-
-}
